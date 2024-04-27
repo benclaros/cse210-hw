a2de6bfc25aebef5e4f6803fd7e56d65ba9e2251
benclaros <claros.benjamin@icloud.com>
2024-04-27 09:59:48 -0400

    Completed part 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What number would be the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+       Console.WriteLine("What is your guess?");
+        int guess = int.Parse(Console.ReadLine());
+        {
+            if (magicNumber > guess)
+        {
+            Console.WriteLine ("Higher");
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine ("Lower");
+        }
+        else 
+        {
+            Console.WriteLine ("You guessed it!");
+        
+        }
+        }
     }
 }
