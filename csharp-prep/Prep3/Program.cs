8e77d2c13855cd585f317dc7a0fcb164e7f40d3f
benclaros <claros.benjamin@icloud.com>
2024-04-27 16:15:11 -0400

    Complete assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,17 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        // For Parts 1 and 2, where the user specified the number...
-        // Console.Write("What is the magic number? ");
-        // int magicNumber = int.Parse(Console.ReadLine());
-        
-        // For Part 3, where we use a random number
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
-
         int guess = -1;
-
-        // We could also use a do-while loop here...
         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
@@ -30,7 +22,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine("YOU DID IT!");
             }
 
         }                    
