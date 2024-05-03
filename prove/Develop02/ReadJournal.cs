eb16699ebdec9127fa04c5171078136b0de6c2dd
benclaros <claros.benjamin@icloud.com>
2024-05-03 15:37:28 -0400

    Complete Assignment

---
+++ prove/Develop02/ReadJournal.cs
@@ -0,0 +1,37 @@
+using System;
+public class ReadJournal{
+    List<Entry1>entrylist = new List<Entry1>();
+    public void writefile(List<Entry1> entrylist){
+      string filename = promptfile($"what is the file name?");
+      using (StreamWriter outputFile = new StreamWriter(filename))
+      foreach (var entry in entrylist){
+
+        outputFile.WriteLine(entry.something());
+
+      }
+    }
+    public string promptfile(string promptquestion){
+      Console.Write(promptquestion);
+      string filename = Console.ReadLine();
+      filename = "journal.txt";
+      return filename;    
+    }
+
+    public List<Entry1>readFile()
+    {
+      string filename = promptfile($"what is the file name?");;
+      string[] lines = System.IO.File.ReadAllLines(filename);
+
+
+        foreach (string line in lines)
+        {
+        string[] parts = line.Split(",");
+        Entry1 write = new Entry1();
+        write.creatlist(parts);
+        entrylist.Add(write);
+
+        }
+        return entrylist;
+
+    }
+}
