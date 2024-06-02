20f3fcab02c6b39c6b222a802a7ac77075250ca6
benclaros <claros.benjamin@icloud.com>
2024-06-01 22:27:47 -0400

    Complete Assignment

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace YouTubeMonitoring
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        static void Main(string[] args)
+        {
+            List<Video> videos = new List<Video>();
+
+            // Create 3-4 videos and add comments
+            Video video1 = new Video("How to clutch in Valorant", "Keznit", 960);
+            video1.AddComment(new Comment("Valo_pro", "That´s the amazing way to clutch"));
+            video1.AddComment(new Comment("Asdasd", "Thanks for sharing!"));
+            video1.AddComment(new Comment("JimmyG", "I learned a lot from this video."));
+            videos.Add(video1);
+
+            Video video2 = new Video("5 special tips about how to heal your teammate", "Mazino, SKT T1", 1280);
+            video2.AddComment(new Comment("Cesar", "Another awesome video Maz"));
+            video2.AddComment(new Comment("V U", "Good as always Maz"));
+            videos.Add(video2);
+
+            Video video3 = new Video("How to become a Pro Player in Valorant", "Horus, TLC", 540);
+            video3.AddComment(new Comment("Aloprolopez", "appreciate the tips."));
+            video3.AddComment(new Comment("FPerez", "Good tips."));
+            video3.AddComment(new Comment("JPablo", "Excellent!"));
+            videos.Add(video3);
+
+            // Iterate through the list of videos and display information
+            foreach (var video in videos)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Title: " + video.Title);
+                Console.WriteLine("Author: " + video.Author);
+                Console.WriteLine("Length (in seconds): " + video.Length);
+                Console.WriteLine("Number of comments: " + video.GetNumberOfComments());
+
+                foreach (var comment in video.GetComments())
+                {
+                    Console.WriteLine("Comment by " + comment.Name + ": " + comment.Text);
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ProductOrderingSystem
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        static void Main(string[] args)
+        {
+            // Create some products
+            Product product1 = new Product("Special Gift Card", "SSD 200-901", 49.99m, 1);
+            Product product2 = new Product("Animated Gun of Clove - Valorant", "LMRT 200-301", 39.99m, 5);
+            Product product3 = new Product("Animated Gun of Phoenix - Valorant", "EXFT 350-401", 47.99m, 2);
+
+            // Create a customer
+            Address address = new Address("698 Lord Chrochane", "Providencia", "Santiago", "Chile");
+            Customer customer = new Customer("Pedro Perez", address);
+
+            // Create an order
+            List<Product> products = new List<Product>();
+            products.Add(product1);
+            products.Add(product2);
+            products.Add(product3);
+            Order order = new Order(customer, products);
+
+            // Output the packing label and shipping label
+            Console.WriteLine("Packing Label:");
+            Console.WriteLine(order.GetPackingLabel());
+            Console.WriteLine("Shipping Label:");
+            Console.WriteLine(order.GetShippingLabel());
+            Console.WriteLine("Total Cost: $" + order.GetTotalCost().ToString("0.00"));
+        }
     }
 }
