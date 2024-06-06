ba4fddccb1ffea7aeb8cb0466581ad084692e73c
benclaros <claros.benjamin@icloud.com>
2024-06-05 23:18:13 -0400

    Complete Assignment

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
+using FitnessCenter;
 
-class Program
+namespace FitnessCenter
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        static void Main(string[] args)
+        {
+            // Create list of activities
+            List<Activity> activities = new List<Activity>();
+
+            // Add some activities to the list
+            activities.Add(new Running(new DateTime(2024, 03, 3), 54, 6.2));
+            activities.Add(new Cycling(new DateTime(2024, 01, 27), 156, 72));
+            activities.Add(new Swimming(new DateTime(2024, 04, 21), 76, 10));
+
+            // Display the summary of each activity
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
 }
